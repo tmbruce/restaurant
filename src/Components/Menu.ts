b9de6ee3205479d94422c3a989db5e88a485c781
Travis <travis.m.bruce@gmail.com>
2022-07-20 22:59:03 -0700

    Styling, layout, add font for menu

--- src/Components/Menu.ts
+++ src/Components/Menu.ts
@@ -1,6 +1,20 @@
+class MenuItem {
+    name: string;
+    price: number;
+    constructor(name: string, price: number) {
+        this.name = name;
+        this.price = price;
+    }
+
+}
+
 const Menu = ((): HTMLDivElement => {
     let menuContainer: HTMLDivElement = document.createElement('div');
     menuContainer.id = 'menu';
+    let title: HTMLHeadingElement = document.createElement('h1');
+    title.className = 'menu-title';
+    title.textContent = 'Jazz De Opus';
+    menuContainer.append(title);
 
     return menuContainer;
 })();
