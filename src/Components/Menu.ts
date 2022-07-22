93edde053b216ad3338f5b53b97bddad3a19f680
Travis <travis.m.bruce@gmail.com>
2022-07-21 22:48:04 -0700

    Work on menu

--- src/Components/Menu.ts
+++ src/Components/Menu.ts
@@ -1,10 +1,27 @@
+import menuItems from "./menuItems";
+
 class MenuItem {
     name: string;
+    description: string;
+    category: string
     price: number;
-    constructor(name: string, price: number) {
+    constructor(name: string, description: string, category: string, price: number) {
         this.name = name;
+        this.description = description;
+        this.category = category;
         this.price = price;
     }
+}
+
+// type items = Object[
+//     {name: 'food',
+//     description: 'type of food',
+//     category: 'pasta',
+//     price: 0}
+//     ];
+
+for (let i = 0; i < menuItems.length; i++) {
+    let food = new MenuItem(menuItems[i].name, menuItems[i].description, menuItems[i].category, menuItems[i].price);
 
 }
 
---
+++ src/Components/menuItems.ts
@@ -0,0 +1,71 @@
+const menuItems = [
+    {
+        name: 'Black Truffle Risotto',
+        description: 'Aged Parmesan, Australian Black Truffles, 3G Shaved Tableside',
+        category: 'Featured Daily Specials',
+        price: 49
+    },
+    {
+        name: 'Mediterranean Octopus',
+        description: 'Braised Octopus, Chickpea Puree, Celery',
+        category: 'Hot Antipasti',
+        price: 24
+    },
+    {
+        name: 'Zucchini Fritti',
+        description: 'Oregano, Red Pepper Almond Dip',
+        category: 'Hot Antipasti',
+        price: 15
+    },
+    {
+        name: 'Squash Blossoms',
+        description: 'Buffalo Mozzarella, Diavolo Sauce',
+        category: 'Hot Antipasti',
+        price: 19
+    },
+    {
+        name: 'Steak Tartare',
+        description: 'Beeman Ranch Sirloin, Tuscan Olive Oil, Shaved Vacche Rosse',
+        category: 'Cold Antipasti',
+        price: 21
+    },
+    {
+        name: 'A5 Wagyu Carpaccio',
+        description: 'Vacche Rosse, Wild Arugula',
+        category: 'Cold Antipasti',
+        price: 38
+    },
+    {
+        name: 'Maine Lobster Caprese',
+        description: 'Buffalo Mozzarella, Basil',
+        category: 'Cold Antipasti',
+        price: 54
+    },
+    {
+        name: 'Carbonara',
+        description: 'Black Pepper Pancetta, Egg Yolk',
+        category: 'Housemade Pasta',
+        price: 19
+    },
+    {
+        name: 'Parmesan Ricotta Gnocchi',
+        description: 'Housemade Gnocchi, Sliced Black Truffle, Cream Sauce',
+        category: 'Housemade Pasta',
+        price: 43
+    },
+    {
+        name: 'Cacio e Pepe',
+        description: 'Toasted Pepper, Pecorino Romano',
+        category: 'Housemade Pasta',
+        price: 19
+    }
+];
+
+export default menuItems;
+
+
+
+// name: string;
+// description: string;
+// category: string
+// price: number;
