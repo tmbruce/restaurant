330c8a0a71777f4bb673dab6a5c363ce049fe910
Travis <travis.m.bruce@gmail.com>
2022-07-22 20:29:25 -0700

    Work on menu display

--- src/Components/Menu.ts
+++ src/Components/Menu.ts
@@ -3,9 +3,9 @@ import menuItems from "./menuItems";
 class MenuItem {
     name: string;
     description: string;
-    category: string
+    category: number
     price: number;
-    constructor(name: string, description: string, category: string, price: number) {
+    constructor(name: string, description: string, category: number, price: number) {
         this.name = name;
         this.description = description;
         this.category = category;
@@ -13,27 +13,63 @@ class MenuItem {
     }
 }
 
-// type items = Object[
-//     {name: 'food',
-//     description: 'type of food',
-//     category: 'pasta',
-//     price: 0}
-//     ];
+const createElement = (type: string, className: string, textContent?: string | number) => {
+    let element = document.createElement(`${type}`);
+    element.className = `${className}`;
+    if (textContent) element.textContent = textContent.toString();
+    return element;
+}
+const createFoodItem = (item: MenuItem): HTMLDivElement  => {
+    const foodItem = createElement('div', 'food-item');
+    const foodName = createElement('h2', 'food-title', item.name);
+    const foodDesc = createElement('h3', 'food-desc', item.description);
+    const foodPrice = createElement('h2', 'food-price', item.price)
+    const foodDetail = createElement('div', 'food-detail');
+    foodDetail.append(foodName, foodDesc);
+    foodItem.append(foodDetail, foodPrice);
+    return <HTMLDivElement>foodItem;
+}
+let cat1: HTMLDivElement[] = [];
+let cat2: HTMLDivElement[] = [];
+let cat3: HTMLDivElement[] = [];
+let cat4: HTMLDivElement[] = [];
 
 for (let i = 0; i < menuItems.length; i++) {
     let food = new MenuItem(menuItems[i].name, menuItems[i].description, menuItems[i].category, menuItems[i].price);
-
+    const foodElement = createFoodItem(food);
+    switch (food.category){
+        case 1:
+            cat1.push(foodElement);
+            break;
+        case 2:
+            cat2.push(foodElement);
+            break;
+        case 3:
+            cat3.push(foodElement);
+            break;
+        case 4:
+            cat4.push(foodElement);
+            break;
+    }
 }
 
-const Menu = ((): HTMLDivElement => {
-    let menuContainer: HTMLDivElement = document.createElement('div');
-    menuContainer.id = 'menu';
-    let title: HTMLHeadingElement = document.createElement('h1');
-    title.className = 'menu-title';
-    title.textContent = 'Jazz De Opus';
-    menuContainer.append(title);
 
-    return menuContainer;
+const Menu = ((): HTMLDivElement => {
+    const menuContainer = createElement('div', 'menu');
+    const title = createElement('h1', 'menu-title', 'Jazz De Opus');
+    menuContainer.append(title);
+    const specials = createElement('h2', 'food-head', 'Daily Special');
+    menuContainer.append(specials, ...cat1);
+    const coldAnti = createElement('div', 'food-head', 'Cold Antipasti');
+    menuContainer.append(coldAnti, ...cat2);
+    const hotAnti = createElement('div', 'food-head', 'Hot Antipasti');
+    menuContainer.append(hotAnti, ...cat3);
+    const housePasta = createElement('div', 'food-head', 'Housemade Pasta');
+    menuContainer.append(housePasta, ...cat4);
+
+
+
+    return <HTMLDivElement>menuContainer;
 })();
 
 export default Menu;
--- src/Components/menuItems.ts
+++ src/Components/menuItems.ts
@@ -1,62 +1,69 @@
-const menuItems = [
+interface menuItems {
+    name: string;
+    description: string;
+    category: number;
+    price: number;
+}
+
+const menuItems: menuItems[] = [
     {
         name: 'Black Truffle Risotto',
         description: 'Aged Parmesan, Australian Black Truffles, 3G Shaved Tableside',
-        category: 'Featured Daily Specials',
+        category: 1,
         price: 49
     },
     {
         name: 'Mediterranean Octopus',
         description: 'Braised Octopus, Chickpea Puree, Celery',
-        category: 'Hot Antipasti',
+        category: 2,
         price: 24
     },
     {
         name: 'Zucchini Fritti',
         description: 'Oregano, Red Pepper Almond Dip',
-        category: 'Hot Antipasti',
+        category: 2,
         price: 15
     },
     {
         name: 'Squash Blossoms',
         description: 'Buffalo Mozzarella, Diavolo Sauce',
-        category: 'Hot Antipasti',
+        category: 2,
         price: 19
     },
     {
         name: 'Steak Tartare',
         description: 'Beeman Ranch Sirloin, Tuscan Olive Oil, Shaved Vacche Rosse',
-        category: 'Cold Antipasti',
+        category: 3,
         price: 21
     },
     {
         name: 'A5 Wagyu Carpaccio',
         description: 'Vacche Rosse, Wild Arugula',
-        category: 'Cold Antipasti',
+        category: 3,
         price: 38
     },
     {
         name: 'Maine Lobster Caprese',
         description: 'Buffalo Mozzarella, Basil',
-        category: 'Cold Antipasti',
+        category: 3,
         price: 54
     },
     {
         name: 'Carbonara',
         description: 'Black Pepper Pancetta, Egg Yolk',
-        category: 'Housemade Pasta',
+        category: 4,
         price: 19
     },
     {
         name: 'Parmesan Ricotta Gnocchi',
         description: 'Housemade Gnocchi, Sliced Black Truffle, Cream Sauce',
-        category: 'Housemade Pasta',
+        category: 4,
         price: 43
     },
     {
         name: 'Cacio e Pepe',
         description: 'Toasted Pepper, Pecorino Romano',
-        category: 'Housemade Pasta',
+        category: 4,
         price: 19
     }
 ];
