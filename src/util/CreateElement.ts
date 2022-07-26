2d358fbce5882d621ab350b470e92bd4124952c3
tmbruce <47762048+tmbruce@users.noreply.github.com>
2022-07-26 15:01:03 -0700

    Delete src directory

--- src/util/CreateElement.ts
+++
@@ -1,8 +0,0 @@
-const createElement = (type: string, className: string, textContent?: string | number) => {
-    let element = document.createElement(`${type}`);
-    element.className = `${className}`;
-    if (textContent) element.textContent = textContent.toString();
-    return element;
-}
-
-export default createElement;
